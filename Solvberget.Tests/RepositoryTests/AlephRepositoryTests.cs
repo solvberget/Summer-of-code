955c800b4e3577053280ce8eb778979367a7abdb
unknown <Arne@windows8test.(none)>
2012-06-18 22:16:35 +0200

    Moved tests into folder

---
+++ Solvberget.Tests/RepositoryTests/AlephRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Solvberget.Domain.Implementation;
+
+namespace Solvberget.Service.Tests.RepositoryTests
+{
+    [TestFixture]
+    public class AlephRepositoryTests
+    {
+
+        [Test]
+        public void TestFind()
+        {
+            const string searchString = "Naiv super";
+            var repository = new AlephRepository();
+            var documents = repository.Search(searchString);
+
+            Assert.AreEqual(12, documents.Count);
+        }
+    }
+}
