b1adb175c80540fd460b012691f23fa7c1ae7707
unknown <capgemini@Z9404315.(none)>
2012-06-19 11:03:23 +0200

    Added dynamic searching

--- Solvberget.Metro/js/DocumentSearch.js
+++ Solvberget.Metro/js/DocumentSearch.js
@@ -3,16 +3,21 @@
     //this.test = ko.observable("Test");
 }
 
-
 function DocumentListViewModel() {
     var self = this;
     self.documents = ko.observableArray([]);
-    self.test = ko.observable("Test");
+    self.searchString = ko.observable("Input search");
 
-    $.getJSON("http://localhost:7089/Document/Search/Naiv super", function(allData) {
-        var mappedDocuments = $.map(allData, function(item) { return new Document(item) });
+    self.search = function () {
+        var url = "http://localhost:7089/Document/Search/" + self.searchString();
+        $.getJSON(url, self.populate);
+    };
+
+    self.populate = function (allData) {
+        var mappedDocuments = $.map(allData, function (item) { return new Document(item) });
         self.documents(mappedDocuments);
-    });
+    };
+
 }
 
 ko.applyBindings(new DocumentListViewModel());
