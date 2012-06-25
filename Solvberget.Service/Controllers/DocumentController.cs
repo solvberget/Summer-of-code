22195db6c9f21304988023317385ad4acb72594a
Sindre <haaland@gmail.com>
2012-06-25 09:17:55 +0200

    Refactor av json navn

--- Solvberget.Service/Controllers/DocumentController.cs
+++ Solvberget.Service/Controllers/DocumentController.cs
@@ -30,7 +30,7 @@ namespace Solvberget.Service.Controllers
             var result = _repository.Search(id);
             return this.Json(result, JsonRequestBehavior.AllowGet);
         }
-        public JsonResult LuceneLookup(string value)
+        public JsonResult SpellingDictionaryLookup(string value)
         {
             var result = _spellingRepository.Lookup(value);
             return this.Json(result, JsonRequestBehavior.AllowGet);
