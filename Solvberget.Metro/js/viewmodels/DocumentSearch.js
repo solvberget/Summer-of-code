48359aa206c1ae2651fca290d64fc76aa464b68b
Sindre <haaland@gmail.com>
2012-06-26 16:04:25 +0200

    Lagt til loading-wheel på søk

--- Solvberget.Metro/js/viewmodels/DocumentSearch.js
+++ Solvberget.Metro/js/viewmodels/DocumentSearch.js
@@ -9,6 +9,7 @@
     self.search = function () {
         var url = "http://localhost:7089/Document/Search/" + self.searchString();
         self.lookup();
+        self.showLoadingWheel(true);
         $.getJSON(url, self.populate);
     };
 
@@ -25,7 +26,8 @@
             return new Document(item);
         });
         self.documents(mappedDocuments);
-        
+        self.showLoadingWheel(false);
+       
     };
 
     self.lookup = function () {
@@ -47,10 +49,52 @@
 
     };
 
-    this.suggestionLink = ko.computed(function () {
+    self.showLoadingWheel = function ( showIt ) {
+
+        if (showIt == undefined || showIt == true) {
+            console.log("Loading wheel showing");
+            var opts = {
+                lines: 15, // The number of lines to draw
+                length: 12, // The length of each line
+                width: 5, // The line thickness
+                radius: 14, // The radius of the inner circle
+                rotate: 30, // The rotation offset
+                color: '#FFF', // #rgb or #rrggbb
+                speed: 1.1, // Rounds per second
+                trail: 45, // Afterglow percentage
+                shadow: false, // Whether to render a shadow
+                hwaccel: false, // Whether to use hardware acceleration
+                className: 'spinner', // The CSS class to assign to the spinner
+                zIndex: 2e9, // The z-index (defaults to 2000000000)
+                top: 'auto', // Top position relative to parent in px
+                left: 'auto' // Left position relative to parent in px
+            };
+
+            var target = jQuery('#searchField');
+            var spinner = new window.Spinner(opts).spin();
+            //var spinner = new window.Spinner().spin();
+            jQuery("#searchField").css("margin-left","200px");
+            jQuery("#searchField").css("margin-top","100px");
+
+
+            target.append(spinner.el);
+        }
+        else {
+            console.log("Loading wheel removed");
+            jQuery('#searchField').html("");
+            jQuery("#searchField").css("margin-left", "0px");
+            jQuery("#searchField").css("margin-top", "0px");
+
+        }
+
+    }
+
+    self.suggestionLink = ko.computed(function () {
         // Will recompute when suggestion is changed
         return "Mente du " + self.suggestion() + "?";
     }, this);
+
+
 
     Windows.ApplicationModel.Search.SearchPane.getForCurrentView().onquerysubmitted = function (eventObject) {
         self.searchString(eventObject.queryText);
