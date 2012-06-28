9ff50d132406a9cb426fdf3cd8a7997a05e4a19e
Tormod Erevik Lea <tormodlea@gmail.com>
2012-06-28 13:41:23 +0200

    Add more repository tests.

--- Solvberget.Tests/RepositoryTests/AlephRepositoryTests.cs
+++ Solvberget.Tests/RepositoryTests/AlephRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using NUnit.Framework;
+using Solvberget.Domain.DTO;
 using Solvberget.Domain.Implementation;
 
 namespace Solvberget.Service.Tests.RepositoryTests
@@ -7,6 +8,8 @@ namespace Solvberget.Service.Tests.RepositoryTests
     [TestFixture]
     public class AlephRepositoryTests
     {
+
+
 
         [Test]
         public void TestFind()
@@ -18,7 +21,44 @@ namespace Solvberget.Service.Tests.RepositoryTests
             var books = documents.Where(x => x.GetType().Name.Equals("Book"));
 
             Assert.AreEqual(7, books.Count());
-            Assert.AreEqual(5, documents.Count(x => x.GetType().Name.Equals("Document")));
+            Assert.AreEqual(3, documents.Count(x => x.GetType().Name.Equals("Document")));
         }
+
+        [Test]
+        public void TestGetDocument()
+        {
+            const string documentNumberForBook = "000596743"; //Naiv. Super
+            const string documentNumberForFilm = "000604643"; //The Matrix
+            const string documentNumberForAudioBook = "000599186"; //Harry Potter og dødtalismanene
+            var repository = new AlephRepository();
+            
+            var book = (Book)repository.GetDocument(documentNumberForBook);
+            Assert.AreEqual("Book", book.GetType().Name);
+            Assert.AreEqual("Naiv. Super", book.Title);
+            Assert.AreEqual("Loe, Erlend", book.Author.Name);
+            Assert.AreEqual("978-82-02-33225-9", book.Isbn);
+            Assert.AreEqual("205 s.", book.NumberOfPages);
+            Assert.AreEqual("LOE", book.LocationCode);
+            Assert.AreEqual(2010, book.PublishedYear);
+            Assert.AreEqual("[Oslo]", book.PlacePublished);
+            
+            var film = (Film)repository.GetDocument(documentNumberForFilm);
+            Assert.AreEqual("Film", film.GetType().Name);
+            Assert.AreEqual("The matrix revolutions", film.Title);
+            Assert.AreEqual("2003", film.ProductionYear);
+            Assert.AreEqual("7321900680592", film.Ean);
+            Assert.IsTrue(film.IsFiction);
+            Assert.AreEqual("mul", film.Language);
+            Assert.AreEqual(2, film.Languages.Count());
+            Assert.AreEqual("eng", film.Languages.ElementAt(0));
+            Assert.AreEqual("ger", film.Languages.ElementAt(1));
+
+            var audioBook = (AudioBook) repository.GetDocument(documentNumberForAudioBook);
+            Assert.AreEqual("AudioBook", audioBook.GetType().Name);
+
+        }
+
+
+
     }
 }
