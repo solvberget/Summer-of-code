73698eb1a486caf754c4db5fb6487dcc59ab3fa7
Tormod Erevik Lea <tormodlea@gmail.com>
2012-07-04 09:22:04 +0200

    Soc48: Add folder for xml-lists and LibraryList poco

---
+++ Solvberget.Domain/DTO/LibraryList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solvberget.Domain.DTO
+{
+    public class LibraryList
+    {
+        public string Name { get; set; }
+        public IEnumerable<string> DocumentNumbers;
+    }
+}
