6c99a2cbb50ee18622a785e66c07513f8c3bfcd5
Kristoffer Rene Eckhoff <kristoffereckhoff@gmail.com>
2012-07-17 10:23:43 +0200

    extended viewmodels, added document viewmodel and cache itemdetailpages

---
+++ Solvberget.Metro/pages/itemDetail/viewmodels/document.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+    WinJS.Namespace.define("ViewModel", {
+        Document: WinJS.Binding.as({
+            documentId: undefined,
+            title: undefined,
+            subtitle: undefined,
+            image: undefined,
+            propertiesList: [],
+            fillProperties: function (documentModel) {
+                this.title = documentModel.Title;
+                this.subtitle="";
+
+            },
+        }),
+
+
+    });
+
+})();
+
