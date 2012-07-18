cddbf1e919afd5cdb8709525520b706740059daf
Kristoffer Rene Eckhoff <kristoffereckhoff@gmail.com>
2012-07-18 14:07:00 +0200

    bokbasen og omtale repository

---
+++ Solvberget.Domain/Implementation/BokbasenRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solvberget.Domain.Implementation
+{
+
+
+    class BokbasenRepository
+    {
+
+        private readonly string _serveruri = string.Empty;
+
+    }
+}
