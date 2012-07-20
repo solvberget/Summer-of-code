3c4be96033cab6e627832be2f170a66cf71554a9
Sindre <haaland@gmail.com>
2012-07-20 12:21:18 +0200

    Major refactor. Gjort om på item details..

---
+++ Solvberget.Metro/fragments/documentFragments/book/book.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    "use strict";
+
+    var page = WinJS.UI.Pages.define("/fragments/documentFragments/book/book.html", {
+        ready: function (element, options) {
+
+
+
+        }
+    });
+
+})();
+
+var fragmentReady = function () {
+
+    console.log("yeah");
+}
+
+
+WinJS.Namespace.define("DocumentDetailFragment", {
+
+    ready: fragmentReady,
+
+});
+
+WinJS.Namespace.define("DocumentDetailConverters", {
+    // Title converter
+    // Subtitle converter
+    // BackgroundImage converter (empty ==> dummy)
+    // 
+});
---
+++ Solvberget.Metro/fragments/documentFragments/film/film.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    // Track if the log in was successful
+    var loggedIn;
+
+    "use strict";
+    var page = WinJS.UI.Pages.define("/fragments/documentFragments/film/film.html", {
+        ready: function (element, options) {
+
+        }
+    });
+
+})();
+
+
+
+var fragmentReady = function () {
+
+    console.log("yeah film");
+}
+
+
+WinJS.Namespace.define("DocumentDetailFragment", {
+
+    ready: fragmentReady,
+
+});
+
+WinJS.Namespace.define("DocumentDetailConverters", {
+    imdbStyleConverter: WinJS.Binding.converter(function (imdbSrc) {
+        return "display:block";
+    }),
+});
