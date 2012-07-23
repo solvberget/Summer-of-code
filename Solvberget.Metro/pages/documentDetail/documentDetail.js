671447cbe27fee04d37a9a702c2bb062b75129a7
Sindre <haaland@gmail.com>
2012-07-23 16:20:16 +0200

    SoC-75 doc placement gui done, margin fix

--- Solvberget.Metro/pages/documentDetail/documentDetail.js
+++ Solvberget.Metro/pages/documentDetail/documentDetail.js
@@ -11,10 +11,76 @@
             documentModel = options.documentModel;
             getDocument(documentModel.DocumentNumber);
 
+            this.registerForShare();
+
         },
+        registerForShare: function () {
+
+            // Register/listen to share requests
+            var dataTransferManager = Windows.ApplicationModel.DataTransfer.DataTransferManager.getForCurrentView();
+            dataTransferManager.addEventListener("datarequested", this.shareHtmlHandler);
+
+            // Open share dialog on openShare-link clicked
+            document.getElementById("openShare").addEventListener("click", function () {
+                Windows.ApplicationModel.DataTransfer.DataTransferManager.showShareUI();
+            });
+
+        },
+        shareHtmlHandler: function (e) {
+
+            var request = e.request;
+
+            // This documents title and img
+            var documentTitle = documentModel.Title;
+
+            if (documentTitle !== "") {
+
+                var range = document.createRange();
+                range.selectNode(document.getElementById("documentShareContent"));
+                request.data = MSApp.createDataPackage(range);
+
+                // Set the title and description of this share-event
+                request.data.properties.title = documentTitle;
+                request.data.properties.description = "Sjekk ut " + documentTitle + " fra bilioteket!";
+
+                var first = true;
+                $("img").each(function (index, item) {
+                    var path = $(this).attr("src");
+                    if (path !== undefined && path !== "undefined") {
+                        var imageUri = new Windows.Foundation.Uri(path);
+                        var streamReference = Windows.Storage.Streams.RandomAccessStreamReference.createFromUri(imageUri);
+                        if (first) {
+                            request.data.setBitmap(streamReference);
+                            first = false;
+                        }
+                        request.data.resourceMap[path] = streamReference;
+                    }
+                });
+
+                var deferral = request.getDeferral();
+                Windows.ApplicationModel.Package.current.installedLocation.getFileAsync("images\\solvberget30.png").then(function (thumbnailFile) {
+                    request.data.properties.thumbnail = Windows.Storage.Streams.RandomAccessStreamReference.createFromFile(thumbnailFile);
+
+                    deferral.complete();
+                }, function (err) {
+                    request.failWithDisplayText(err);
+                });
+
+
+            } else {
+
+                request.failWithDisplayText("Hmm, vi fant faktisk ingen tittel å dele");
+
+            }
+
+        },
+
     });
 
 })();
+
+
+
 
 
 var documentModel = undefined;
@@ -116,6 +182,8 @@ var getDocument = function (documentNumber) {
                 var documentTitleDiv = document.getElementById("documentTitle");
                 var documentImageDiv = document.getElementById("documentImage");
                 var documentSubTitleDiv = document.getElementById("item-subtitle");
+                var documentShareContent = document.getElementById("documentShareContent");
+
 
                 // avoid processing null (if user navigates to fast away from page etc)
                 if (documentTitleDiv != undefined && response != undefined)
@@ -124,6 +192,8 @@ var getDocument = function (documentNumber) {
                     WinJS.Binding.processAll(documentImageDiv, response);
                 if (documentSubTitleDiv != undefined && response != undefined)
                     WinJS.Binding.processAll(documentSubTitleDiv, response);
+                if (documentShareContent != undefined && response != undefined)
+                    WinJS.Binding.processAll(documentShareContent, response);
 
 
             }
@@ -148,14 +218,14 @@ WinJS.Namespace.define("DocumentDetailConverters", {
         return "/images/placeholders/" + documentModel.DocType + ".png";
 
     }),
-    docAvailableStyle: WinJS.Binding.converter(function(numAvailable) {
+    docAvailableStyle: WinJS.Binding.converter(function (numAvailable) {
         return (!numAvailable || numAvailable < 1 || numAvailable == "0" || numAvailable == "null") ? "Red" : "Green";
     }),
     docAvailable: WinJS.Binding.converter(function (numAvailable) {
         return numAvailable + " av";
     }),
     docTotalCount: WinJS.Binding.converter(function (totalCount) {
-        var pluralFix = totalCount  < 2 ? "eksemplar" : "eksemplarer";
+        var pluralFix = totalCount < 2 ? "eksemplar" : "eksemplarer";
         return totalCount + " " + pluralFix + " tilgjengelig";
     }),
     docEarliestAvailableDate: WinJS.Binding.converter(function (date) {
