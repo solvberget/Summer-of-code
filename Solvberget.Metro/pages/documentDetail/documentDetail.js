4d55e91236ddd3686c2d95dec1e08d8ed1dfa3de
Sindre <haaland@gmail.com>
2012-07-24 09:51:05 +0200

    La til headernavigasjon på alle sider

--- Solvberget.Metro/pages/documentDetail/documentDetail.js
+++ Solvberget.Metro/pages/documentDetail/documentDetail.js
@@ -12,8 +12,38 @@
             getDocument(documentModel.DocumentNumber);
 
             this.registerForShare();
+            var self = this;
+            element.querySelector(".titlearea").addEventListener("click", this.showHeaderMenu, false);
+            document.getElementById("headerMenuLists").addEventListener("click", function () { window.Data.itemByKey("lists").navigateTo(); }, false);
+            document.getElementById("headerMenuEvents").addEventListener("click", function () { window.Data.itemByKey("events").navigateTo(); }, false);
+            document.getElementById("headerMenuMyPage").addEventListener("click", function () { window.Data.itemByKey("mypage").navigateTo(); }, false);
+            document.getElementById("headerMenuSearch").addEventListener("click", function () { window.Data.itemByKey("search").navigateTo(); }, false);
+            document.getElementById("headerMenuHomeMenuItem").addEventListener("click", function () { self.goHome(); }, false);
+
+            var theMenu = document.getElementById("HeaderMenu");
+            WinJS.UI.processAll(theMenu);
+
 
         },
+
+
+        showHeaderMenu: function () {
+
+            var title = document.querySelector("header .titlearea");
+            var menu = document.getElementById("HeaderMenu").winControl;
+            menu.anchor = title;
+            menu.placement = "bottom";
+            menu.alignment = "left";
+
+            menu.show();
+
+        },
+
+        goHome: function () {
+            WinJS.Navigation.navigate("/pages/home/home.html");
+
+        },
+      
         registerForShare: function () {
 
             // Register/listen to share requests
