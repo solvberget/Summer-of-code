24848cdb3bb0cc3d44c3a5483f8fb33a3dce5dfc
Sindre <haaland@gmail.com>
2012-07-31 14:09:58 +0200

    BloggRepository, tests, dto

---
+++ Solvberget.Domain/DTO/BlogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solvberget.Domain.DTO
+{
+    class BlogEntry
+    {
+
+        public static BlogEntry FillItem(string xml, string type)
+        {
+            return new BlogEntry();
+        }
+
+    }
+}
