22b2fe39470ea1d3324ae5bd49ab5a677968639e
Sindre <haaland@gmail.com>
2012-08-06 14:41:08 +0200

    SoC-88 Blogger Gui ferdig

--- Solvberget.Metro/pages/blogs/entry/entry.js
+++ Solvberget.Metro/pages/blogs/entry/entry.js
@@ -1,45 +1,27 @@
 ﻿(function () {
     "use strict";
-
-    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
-    var binding = WinJS.Binding;
-    var nav = WinJS.Navigation;
-    var utils = WinJS.Utilities;
+    
     var ui = WinJS.UI;
 
     ui.Pages.define("/pages/blogs/entry/entry.html", {
 
         ready: function (element, options) {
+            var model = options.model;
+            // Sanitize HTML (remove dynamic HTML like iframes and script)
+            model.Content = window.toStaticHTML(model.Content);
+            
+            // Float images inside the text 
+            WinJS.Binding.processAll(element, model).done(function () {
+                $("a img").each(function () {
+                    if(!$(this).parent().parent().hasClass("separator")) // Bugfix with blogger.com
+                         $(this).parent().css("float", "left").css("padding", "10px");
+                });
 
-            // TODO: Check this 
-            populateEntry(options.entry);
-
-        },
-
-        goHome: function () {
-            WinJS.Navigation.navigate("/pages/home/home.html");
-
+            });
         },
     });
-
 })();
 
 
-var populateEntry = function (entry) {
 
-    var entryTemplateDiv = document.getElementById("blogTemplate");
-    var entryTemplateHolder = document.getElementById("entryTemplateHolder");
-
-    var blogTemplate = undefined;
-    if (entryTemplateDiv)
-        blogTemplate = new WinJS.Binding.Template(entryTemplateDiv);
-
-    var model;
-
-    if (entry) {
-
-        blogTemplate.render(entry, entryTemplateHolder);          
-    }
-
-};
 
