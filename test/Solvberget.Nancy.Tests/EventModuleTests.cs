e8bc9343d29692d319b6b0ea9d76634f2c1c13e7
Kristian Hellang <kristian@hellang.com>
2013-10-17 17:49:56 +0200

    Added some tests for the most simple Nancy modules

---
+++ test/Solvberget.Nancy.Tests/EventModuleTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using FakeItEasy;
+
+using Nancy.Testing;
+
+using Should;
+
+using Solvberget.Domain.Abstract;
+using Solvberget.Domain.DTO;
+using Solvberget.Nancy.Modules;
+
+using Xunit;
+
+namespace Solvberget.Nancy.Tests
+{
+    public class EventModuleTests
+    {
+        private readonly IEventRepository _repository;
+
+        private readonly Browser _browser;
+
+        public EventModuleTests()
+        {
+            _repository = A.Fake<IEventRepository>();
+            _browser = new Browser(config =>
+            {
+                config.Module<EventModule>();
+                config.Dependency(_repository);
+            });
+        }
+
+        [Fact]
+        public void GetShouldFetchAllEventsFromRepository()
+        {
+            // Given
+            A.CallTo(() => _repository.GetEvents()).Returns(new List<Event>
+            {
+                new Event { Name = "Awesome Event 1", DateAsString = "10-10-2013" },
+                new Event { Name = "Awesome Event 2", DateAsString = "08-12-2013" }
+            });
+
+            // When
+            var response = _browser.Get("/events", with =>
+            {
+                with.Accept("application/json");
+                with.HttpRequest();
+            });
+
+            // Then
+            response.Body.DeserializeJson<List<Event>>().Count.ShouldEqual(2);
+        }
+    }
+}
---
+++ test/Solvberget.Nancy.Tests/NewsModuleTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+using FakeItEasy;
+
+using Nancy.Testing;
+
+using Should;
+
+using Solvberget.Domain.Abstract;
+using Solvberget.Domain.DTO;
+using Solvberget.Nancy.Modules;
+
+using Xunit;
+
+namespace Solvberget.Nancy.Tests
+{
+    public class NewsModuleTests
+    {
+        private readonly INewsRepository _repository;
+
+        private readonly Browser _browser;
+
+        public NewsModuleTests()
+        {
+            _repository = A.Fake<INewsRepository>();
+            _browser = new Browser(config =>
+            {
+                config.Module<NewsModule>();
+                config.Dependency(_repository);
+            });
+        }
+
+        [Fact]
+        public void GetShouldFetchNewsItemsFromRepository()
+        {
+            // Given
+            A.CallTo(() => _repository.GetNewsItems(30)).Returns(new List<NewsItem>
+            {
+                new NewsItem
+                {
+                    Title = "Google Offers Android app gets a new look and simpler redemption process.",
+                    DescriptionUnescaped = "Blah blah blah..."
+                },
+                new NewsItem
+                {
+                    Title = "T-Mobile launches trio of budget Android smartphones and an LTE hotspot.",
+                    DescriptionUnescaped = "Blah blah blah..."
+                }
+            });
+
+            // When
+            var response = _browser.Get("/news", with =>
+            {
+                with.Query("limit", "30");
+                with.Accept("application/json");
+                with.HttpRequest();
+            });
+
+            // Then
+            response.Body.DeserializeJson<List<NewsItem>>().Count.ShouldEqual(2);
+        }
+    }
+}
