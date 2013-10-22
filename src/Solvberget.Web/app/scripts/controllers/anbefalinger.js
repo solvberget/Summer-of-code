3a696d2effbeb0d2472fcca74513a3a75f92e4ee
Fredrik Kalseth <fredrik@kalseth.com>
2013-10-22 14:23:49 +0200

    Created menu and hooked up menu items to (empty) views

---
+++ src/Solvberget.Web/app/scripts/controllers/anbefalinger.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('Solvberget.WebApp')
+  .controller('AnbefalingerCtrl', function ($scope) {
+   
+
+  });
---
+++ src/Solvberget.Web/app/scripts/controllers/minside.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('Solvberget.WebApp')
+  .controller('MinSideCtrl', function ($scope) {
+   
+
+  });
