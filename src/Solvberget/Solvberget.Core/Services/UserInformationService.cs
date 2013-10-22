ca9b7c5736b633fafdcede15766fdb31e5fd368c
Ellen Wiig Andresen <ellenwiig@gmail.com>
2013-10-22 11:55:06 +0200

    ryddet litt

---
+++ src/Solvberget/Solvberget.Core/Services/UserInformationService.cs
@@ -0,0 +1,20 @@
+﻿using Solvberget.Domain.DTO;
+
+namespace Solvberget.Core.Services
+{
+    interface IUserInformationService
+    {
+        UserInfo GetUserInformation(string userId);
+    }
+
+    class UserInformationService : IUserInformationService
+    {
+        public UserInfo GetUserInformation(string userId)
+        {
+            return new UserInfo()
+            {
+                Name = "Ellen Wiig Andresen"
+            };
+        }
+    }
+}
