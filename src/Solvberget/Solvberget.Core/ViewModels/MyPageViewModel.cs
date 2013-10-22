2e2ff2296a87dbfda7ab0813a1aa8952ed02eae9
Ellen Wiig Andresen <ellenwiig@gmail.com>
2013-10-22 11:52:21 +0200

    fikser VS

--- src/Solvberget/Solvberget.Core/ViewModels/MyPageViewModel.cs
+++ src/Solvberget/Solvberget.Core/ViewModels/MyPageViewModel.cs
@@ -1,8 +1,48 @@
-﻿using Solvberget.Core.ViewModels.Base;
+﻿using System;
+using Cirrious.CrossCore;
+using Solvberget.Core.Services;
+using Solvberget.Core.ViewModels.Base;
+using Solvberget.Domain.DTO;
 
 namespace Solvberget.Core.ViewModels
 {
-    public class MyPageViewModel : BaseViewModel 
+    public class MyPageViewModel : BaseViewModel
     {
+        public MyPageViewModel()
+        {
+            var service = Mvx.Resolve<IUserInformationService>();
+            if (service == null) throw new ArgumentNullException("service");
+
+            service.GetUserInformation("id");
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                RaisePropertyChanged(() => Email);
+            }
+        }
+
     }
 }
