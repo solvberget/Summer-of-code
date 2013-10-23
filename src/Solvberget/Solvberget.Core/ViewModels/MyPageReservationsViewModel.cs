9e5e54eacb794fb7416f1615c8a3dd626505ad6b
Ellen Wiig Andresen <ellenwiig@gmail.com>
2013-10-23 15:33:10 +0200

    add files

---
+++ src/Solvberget/Solvberget.Core/ViewModels/MyPageReservationsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cirrious.CrossCore;
+using Solvberget.Core.Services;
+using Solvberget.Core.ViewModels.Base;
+using Solvberget.Domain.DTO;
+
+namespace Solvberget.Core.ViewModels
+{
+    public class MyPageReservationsViewModel : BaseViewModel
+    {
+        public MyPageReservationsViewModel()
+        {
+            var service = Mvx.Resolve<IUserInformationService>();
+            if (service == null) throw new ArgumentNullException("service");
+
+            Reservations = service.GetUserReservations("id");
+        }
+
+        private List<Reservation> _reservations;
+        public List<Reservation> Reservations
+        {
+            get
+            {
+                return _reservations;
+            }
+            set
+            {
+                _reservations = value;
+                RaisePropertyChanged(() => Reservations);
+            }
+        }
+    }
+}
