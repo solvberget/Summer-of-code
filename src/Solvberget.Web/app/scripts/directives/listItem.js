a5bf703cd0a1a00cfc98f78008869e328d757378
Fredrik Kalseth <fredrik@kalseth.com>
2013-10-24 17:02:04 +0200

    Bootstrap 3

--- src/Solvberget.Web/app/scripts/directives/listItem.js
+++ src/Solvberget.Web/app/scripts/directives/listItem.js
@@ -14,10 +14,11 @@ angular.module('Solvberget.WebApp')
             },
             replace:true,
             template: "<li class='media' ng-click='open()'>" +
-                        "<span><img class='media-object' src='{{imageUrl}}' alt='{{title}}'></span>" +
+                        "<span><img class='media-object' ng-src='{{imageUrl}}' alt='{{title}}'></span>" +
                         "<div class='media-body'>" +
                             "<h4 class='media-heading'>{{title}}</h4>" +
                         "</div>" +
+                        "<span class='list-item-actions'><span class='.glyphicon .glyphicon-star' /></span>" +
                       "</li>"
         };
     });
