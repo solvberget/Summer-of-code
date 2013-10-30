c196cbfde2acab84fc22a75e2d57569bd74ff440
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-10-30 16:36:29 +0100

    Adding viewmodels for contact informatin

---
+++ src/Solvberget/Solvberget.Core/ViewModels/ContactInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Solvberget.Core.ViewModels.Base;
+
+namespace Solvberget.Core.ViewModels
+{
+    public class ContactInfoViewModel : BaseViewModel
+    {
+        private List<ContactInfoBoxViewModel> _infoBoxes;
+        public List<ContactInfoBoxViewModel> InfoBoxes 
+        {
+            get { return _infoBoxes; }
+            set { _infoBoxes = value; RaisePropertyChanged(() => InfoBoxes);}
+        }
+    }
+}
---
+++ src/Solvberget/Solvberget.Core/ViewModels/ContactPersonViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Solvberget.Core.ViewModels.Base;
+
+namespace Solvberget.Core.ViewModels
+{
+    public class ContactPersonViewModel : BaseViewModel
+    {
+        private string _name;
+        public string Name 
+        {
+            get { return _name; }
+            set { _name = value; RaisePropertyChanged(() => Name);}
+        }
+
+        private string _position;
+        public string Position 
+        {
+            get { return _position; }
+            set { _position = value; RaisePropertyChanged(() => Position);}
+        }
+
+        private string _email;
+        public string Email 
+        {
+            get { return _email; }
+            set { _email = value; RaisePropertyChanged(() => Email);}
+        }
+
+        private string _phone;
+        public string Phone 
+        {
+            get { return _phone; }
+            set { _phone = value; RaisePropertyChanged(() => Phone);}
+        }
+    }
+}
