b4ad338f1c9e9f26d7f9e45370299522a9871fc5
Fredrik Kalseth <fredrik@kalseth.com>
2013-10-30 12:15:58 +0100

    Bruk rootscope i views men konfig objekt i js

--- src/Solvberget.Web/app/scripts/controllers/media.detaljer.js
+++ src/Solvberget.Web/app/scripts/controllers/media.detaljer.js
@@ -4,7 +4,7 @@ var mediaDetaljerCtrl = function ($scope, $rootScope, $routeParams, documents, d
 
     $scope.document = documents.get({id : $routeParams.id}, function(){
         $rootScope.breadcrumb.push($scope.document.title);
-        $scope.imageUrl = $rootScope.apiPrefix + 'documents/' + $scope.document.id + '/thumbnail';
+        $scope.imageUrl = $$config.apiPrefix + 'documents/' + $scope.document.id + '/thumbnail';
     });
 
     $scope.rating = documentRating.get({id : $routeParams.id}, function(){
