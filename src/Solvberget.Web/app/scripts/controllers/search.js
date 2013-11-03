c476192a1d06799d2ea2161668d2cc4caff7cc15
Fredrik Kalseth <fredrik@kalseth.com>
2013-11-03 13:15:59 +0100

    Søk implementert med filtrering på type etc.

--- src/Solvberget.Web/app/scripts/controllers/search.js
+++ src/Solvberget.Web/app/scripts/controllers/search.js
@@ -1,20 +1,34 @@
 'use strict';
 
 angular.module('Solvberget.WebApp')
-    .controller('SearchCtrl', function ($scope, $rootScope, documentSearch) {
+    .controller('SearchCtrl', function ($scope, $rootScope, $filter, documentSearch) {
 
         $rootScope.breadcrumb.clear();
         $rootScope.breadcrumb.push('Søk');
 
+        $scope.selectedType = null;
+
+        $scope.selectType = function(type){
+            $scope.selectedType = type;
+
+            if(null == type) delete $scope.resultFilter;
+            else $scope.resultFilter = {type : type};
+        }
+
         $scope.search = function(){
 
-            console.log("Searching for " + $scope.searchQuery);
             $scope.isSearching = true;
 
             $scope.results = documentSearch.query({query : $scope.searchQuery}, function(){
 
+                $scope.allCount = $scope.results.length;
+                $scope.bookCount = $scope.results.filter(function(item){ return item.type == 'Book'}).length;
+                $scope.filmCount = $scope.results.filter(function(item){ return item.type == 'Film'}).length;
+                $scope.audioBookCount = $scope.results.filter(function(item){ return item.type == 'AudioBook'}).length;
+                $scope.cdCount = $scope.results.filter(function(item){ return item.type == 'CD'}).length;
+                $scope.sheetMusicCount = $scope.results.filter(function(item){ return item.type == 'SheetMusic'}).length;
+
                 $scope.isSearching = false;
-                console.log("search results fetched", $scope.results);
             });
         }
 
