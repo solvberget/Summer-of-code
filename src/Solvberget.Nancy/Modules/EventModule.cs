75cb92339196f761889ec39532cde92197e5d508
Fredrik Kalseth <fredrik@kalseth.com>
2013-11-05 14:31:38 +0100

    Nancy service for arrangementer. Testdata foreløpig, mangler integrasjon med ny ticket provider..

--- src/Solvberget.Nancy/Modules/EventModule.cs
+++ src/Solvberget.Nancy/Modules/EventModule.cs
@@ -1,5 +1,9 @@
-﻿using Nancy;
+﻿using System;
+using System.Linq;
+using Nancy;
+using Solvberget.Core.DTOs;
 using Solvberget.Domain.Events;
+using Solvberget.Nancy.Mapping;
 
 namespace Solvberget.Nancy.Modules
 {
@@ -7,7 +11,26 @@ namespace Solvberget.Nancy.Modules
     {
         public EventModule(IEventRepository eventRepository) : base("/events")
         {
-            Get["/"] = _ => eventRepository.GetEvents();
+            var testEvents = new[]
+            {
+                new EventDto
+                {
+                    Id = 1,
+                    Name = "Testarrangement", Description = "Testbeskrivelse",
+                    Start = new DateTime(2013,11,30,15,00,00), End = new DateTime(2013,11,30, 16,00,00)
+                },
+                new EventDto
+                {
+                    Id = 2,
+                    Name = "Julaften", Description = "Gaver til alle de snille barna",
+                    Start = new DateTime(2013,12,24), End = new DateTime(2013,12,24), AllDay=true
+                }
+            };
+
+            // todo: implement after new events integration in place
+            Get["/"] = _ => testEvents;
+
+            Get["/{id}"] = args => testEvents.FirstOrDefault(ev => ev.Id == args.id);
         }
     }
 }
