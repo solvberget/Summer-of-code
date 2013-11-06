5ae13a3d9f5b2322e5e6d3eda3d5231c94a2486d
Fredrik Kalseth <fredrik@kalseth.com>
2013-11-06 14:59:43 +0100

    Implementert enkel Aleph auth med Nancy Stateless Authentication. Lagt til LoginModule.

---
+++ src/Solvberget.Nancy/Authentication/AlephUserIdentity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Nancy.Security;
+using Solvberget.Domain.Users;
+
+namespace Solvberget.Nancy.Authentication
+{
+    public class AlephUserIdentity : IUserIdentity
+    {
+        public AlephUserIdentity(string username, UserInfo userInfo)
+        {
+            UserName = username;
+            UserInfo = userInfo;
+        }
+
+        public UserInfo UserInfo { get; private set; }
+
+        public string UserName { get; private set; }
+        public IEnumerable<string> Claims { get; private set; }
+    }
+}
---
+++ src/Solvberget.Nancy/Authentication/IAuthenticationProvider.cs
@@ -0,0 +1,9 @@
+﻿using Nancy.Security;
+
+namespace Solvberget.Nancy.Authentication
+{
+    public interface IAuthenticationProvider
+    {
+        IUserIdentity Authenticate(string username, string password);
+    }
+}
