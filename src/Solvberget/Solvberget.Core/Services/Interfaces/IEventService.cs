e15973a3e104dbb7086128b8917a3173af002eb8
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-11-14 00:23:50 +0100

    Adding client side service for fetching events

---
+++ src/Solvberget/Solvberget.Core/Services/Interfaces/IEventService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Solvberget.Core.DTOs;
+
+namespace Solvberget.Core.Services.Interfaces
+{
+    public interface IEventService
+    {
+        Task<IList<EventDto>> GetList();
+        Task<EventDto> Get(int eventId);
+    }
+}
