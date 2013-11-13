f9ed17e1be3ceda762b982afdaf7322614ae390f
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-11-13 10:09:58 +0100

    Adding home view and references to mvvm projects

---
+++ src/Solvberget/Solvberget.iOS/Views/HomeView.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Solvberget.Core.ViewModels;
+using Cirrious.MvvmCross.Touch.Views;
+
+namespace Solvberget.iOS
+{
+    public partial class HomeView : MvxViewController
+    {
+        public new HomeViewModel ViewModel
+        {
+            get { return (HomeViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
+
+        public HomeView()
+			: base (UserInterfaceIdiomIsPhone ? "HomeView_iPhone" : "HomeView_iPad", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+			
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
+
