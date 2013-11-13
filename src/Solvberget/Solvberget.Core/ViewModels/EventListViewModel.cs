73e34fac3130f7aaf05fa46ac9053b4c3c3f5d4e
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-11-14 00:48:17 +0100

    Event listing now renders list of events and links them to a webview

--- src/Solvberget/Solvberget.Core/ViewModels/EventListViewModel.cs
+++ src/Solvberget/Solvberget.Core/ViewModels/EventListViewModel.cs
@@ -1,15 +1,58 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+using Solvberget.Core.Services.Interfaces;
 using Solvberget.Core.ViewModels.Base;
+using System.Linq;
 
 namespace Solvberget.Core.ViewModels
 {
     public class EventListViewModel : BaseViewModel
     {
-        private List<EventViewModel> _event;
-        public List<EventViewModel> Event 
+        private readonly IEventService _eventService;
+
+        public EventListViewModel(IEventService eventService)
         {
-            get { return _event; }
-            set { _event = value; RaisePropertyChanged(() => Event);}
+            _eventService = eventService;
+        }
+
+        public void Init()
+        {
+            Load();
+        }
+
+        private async void Load()
+        {
+            IsLoading = true;
+
+            Events = (from ev in await _eventService.GetList()
+                select new EventViewModel
+                {
+                    Title = ev.Name
+                }).ToList();
+
+            IsLoading = false;
+        }
+
+        private List<EventViewModel> _events;
+        public List<EventViewModel> Events 
+        {
+            get { return _events; }
+            set { _events = value; RaisePropertyChanged(() => Events);}
+        }
+
+        private MvxCommand<EventViewModel> _showDetailsCommand;
+        public ICommand ShowDetailsCommand
+        {
+            get
+            {
+                return _showDetailsCommand ?? (_showDetailsCommand = new MvxCommand<EventViewModel>(ExecuteShowDetailsCommand));
+            }
+        }
+
+        private void ExecuteShowDetailsCommand(EventViewModel ev)
+        {
+            ShowViewModel<GenericWebViewViewModel>(new { uri = ev.Uri, title = ev.Title });
         }
     }
 }
--- src/Solvberget/Solvberget.Core/ViewModels/EventViewModel.cs
+++ src/Solvberget/Solvberget.Core/ViewModels/EventViewModel.cs
@@ -4,5 +4,11 @@ namespace Solvberget.Core.ViewModels
 {
     public class EventViewModel : BaseViewModel
     {
+        private string _uri;
+        public string Uri 
+        {
+            get { return _uri; }
+            set { _uri = value; RaisePropertyChanged(() => Uri);}
+        }
     }
 }
