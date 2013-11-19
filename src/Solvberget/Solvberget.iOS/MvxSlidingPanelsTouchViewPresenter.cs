71732ab3321273c7846c657201f537657bffcae1
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-11-19 23:40:59 +0100

    Addingand binding initial opening hours list

--- src/Solvberget/Solvberget.iOS/MvxSlidingPanelsTouchViewPresenter.cs
+++ src/Solvberget/Solvberget.iOS/MvxSlidingPanelsTouchViewPresenter.cs
@@ -1,3 +1,6 @@
+// 
+// As seen in SlidingPanels.Touch sample project 
+//
 using System;
 using Cirrious.MvvmCross.Touch.Views.Presenters;
 using MonoTouch.UIKit;
@@ -13,26 +16,10 @@ using Solvberget.Core.ViewModels;
 
 namespace Solvberget.iOS
 {
-    /// <summary>
-    /// View Presenter for this application
-    /// Manages automatically creating the Sliding panel view controller and its 
-    /// requisite panel views as well as showing normal views
-    /// </summary>
     public class MvxSlidingPanelsTouchViewPresenter : MvxTouchViewPresenter
     {
-        #region Data Members
-
-        /// <summary>
-        /// the application's window.  We hang onto this so we can use it later
-        /// </summary>
         private UIWindow _window;
 
-        #endregion
-
-        /// <summary>
-        /// Gets the Sliding Panels View Controller
-        /// </summary>
-        /// <value>The nav controller.</value>
         public SlidingPanelsNavigationViewController SlidingPanelsController 
         {
             get
@@ -41,32 +28,18 @@ namespace Solvberget.iOS
             }
         }
 
-        /// <summary>
-        /// Gets the root controller for the application
-        /// </summary>
-        /// <value>The root controller.</value>
         public UIViewController RootController 
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MvxSlidingPanels.Touch.MvxSlidingPanelsTouchViewPresenter"/> class.
-        /// </summary>
-        /// <param name="applicationDelegate">Application delegate.</param>
-        /// <param name="window">Window.</param>
         public MvxSlidingPanelsTouchViewPresenter(UIApplicationDelegate applicationDelegate, UIWindow window) :
             base(applicationDelegate, window)
         {
-            // specialized construction logic goes here
             _window = window;
         }
 
-        /// <summary>
-        /// Override this when custom presentation logic is needed from your view models
-        /// </summary>
-        /// <param name="hint">Hint.</param>
         public override void ChangePresentation (Cirrious.MvvmCross.ViewModels.MvxPresentationHint hint)
         {
             base.ChangePresentation(hint);
@@ -92,15 +65,7 @@ namespace Solvberget.iOS
 			MasterNavigationController.PopToRootViewController (true);
 			MasterNavigationController = null;
 		}
-
-
-        /// <summary>
-        /// Shows the first view.
-        /// This method has been overridden to automatically create the sliding panels view controller.
-        /// Also, for each of the sliding panels (left, right and bottom) a View/View Model is created
-        /// and set into a panel container managed by the sliding panel view controller.
-        /// </summary>
-        /// <param name="viewController">View controller.</param>
+	
         protected override void ShowFirstView (UIViewController viewController)
         {
             // Show the first view
@@ -113,32 +78,12 @@ namespace Solvberget.iOS
 
             // use the first view to create the sliding panels 
 			AddPanel<HomeViewModel>(PanelType.LeftPanel, viewController as MvxViewController);
-           // AddPanel<RightPanelViewModel>(PanelType.RightPanel, viewController as MvxViewController);
-           // AddPanel<BottomPanelViewModel>(PanelType.BottomPanel, viewController as MvxViewController);
         }
 
-        /// <summary>
-        /// Creates a ViewModel of the desired type (as well as the  ppropriate View) and inserts it into
-        /// the correct Panel Container and inserts the panel container into the provided sliding panel
-        /// view controller
-        /// </summary>
-        /// <param name="panelType">Panel type.</param>
-        /// <param name="mvxController">Mvx controller.</param>
-        /// <typeparam name="T">The 1st type parameter.</typeparam>
         protected void AddPanel<T>(PanelType panelType, MvxViewController mvxController) where T : MvxViewModel
         {
-
-            // use the first view to create a view of the desired type
-            // We only do this because there's no convenient way to create a view from inside the presenter
-
-            // TODO:  According to Stuart, We c(sh)ould be doing something like this instead:
-            // var parameterBundle = new MvxBundle(null);
-            // var request = new MvxViewModelRequest<TTargetViewModel>(parameterBundle, null, MvxRequestedBy.UserAction);
-            // return Mvx.Resolve<IMvxTouchViewCreator>().CreateView(request);
             UIViewController viewToAdd = (UIViewController) mvxController.CreateViewControllerFor<T>();
 
-            // Insert the view into a new container (of the right type) and insert 
-            // that into the sliding panels controller
             switch (panelType)
             {
                 case PanelType.LeftPanel:
@@ -158,12 +103,6 @@ namespace Solvberget.iOS
             };
         }
 
-        /// <summary>
-        /// Overrides the default presenters method so that we can instead create a sliding panel
-        /// view controller and use that as our application navigation view controller
-        /// </summary>
-        /// <returns>The navigation controller.</returns>
-        /// <param name="viewController">View controller.</param>
         protected override UINavigationController CreateNavigationController (UIViewController viewController)
         {
             SlidingPanelsNavigationViewController navController = new SlidingPanelsNavigationViewController (viewController);
@@ -171,10 +110,6 @@ namespace Solvberget.iOS
             return navController;
         }
 
-        /// <summary>
-        /// Sets the window root view controller.
-        /// </summary>
-        /// <param name="controller">Controller.</param>
         protected override void SetWindowRootViewController(UIViewController controller)
         {
             _window.AddSubview(RootController.View);
---
+++ src/Solvberget/Solvberget.iOS/Views/OpeningHoursView.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Cirrious.MvvmCross.Touch.Views;
+using Solvberget.Core.ViewModels;
+using Cirrious.MvvmCross.Binding.Touch.Views;
+using Cirrious.MvvmCross.Binding.BindingContext;
+
+namespace Solvberget.iOS
+{
+	public partial class OpeningHoursView : MvxTableViewController
+    {
+		public new OpeningHoursViewModel ViewModel
+		{
+			get
+			{
+				return base.ViewModel as OpeningHoursViewModel;
+			}
+		}
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+			
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			
+            // Perform any additional setup after loading the view, typically from a nib.
+			var source = new MvxStandardTableViewSource(TableView, UITableViewCellStyle.Subtitle, new NSString("TableViewCell"), "TitleText Title; DetailText UrlText", UITableViewCellAccessory.None);
+			TableView.Source = source;
+
+			var loadingIndicator = new LoadingOverlay(View.Frame);
+			Add(loadingIndicator);
+
+			var set = this.CreateBindingSet<OpeningHoursView, OpeningHoursViewModel>();
+			set.Bind(source).To(vm => vm.Locations);
+			set.Bind(source).For(s => s.SelectionChangedCommand).To(vm => vm.ShowDetailsCommand);
+			set.Bind().For(v => v.Title).To(vm => vm.Title);
+			set.Bind(loadingIndicator).For("Visibility").To(vm => vm.IsLoading).WithConversion("Visibility");
+			set.Apply();
+
+			TableView.ReloadData();
+        }
+    }
+}
+
