ecf04e35ed553b5ab0fe908d5823ba63c05f76ed
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-12-02 17:22:11 +0100

    Blog post details now show in a local web view so we can display images

---
+++ src/Solvberget/Solvberget.Droid/Views/Fragments/LocalHtmlWebView.cs
@@ -0,0 +1,49 @@
+using Android.App;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Cirrious.MvvmCross.Binding;
+using Cirrious.MvvmCross.Binding.BindingContext;
+using Solvberget.Core.ViewModels;
+using Solvberget.Droid.ActionBar;
+using Solvberget.Droid.Views.WebClients;
+
+namespace Solvberget.Droid.Views.Fragments
+{
+    [Activity(Label = "Webside", Theme = "@style/MyTheme", Icon = "@android:color/transparent", ParentActivity = typeof(HomeView))]
+    [MetaData("android.support.PARENT_ACTIVITY", Value = "solvberget.droid.views.HomeView")]
+    public class LocalHtmlWebView : MvxActionBarActivity
+    {
+        private WebView _webView;
+
+        protected override void OnViewModelSet()
+        {
+            Window.RequestFeature(WindowFeatures.Progress);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetHomeButtonEnabled(true);
+
+            base.OnViewModelSet();
+
+            SetContentView(Resource.Layout.page_localwebview);
+
+
+            var set = this.CreateBindingSet<LocalHtmlWebView, LocalHtmlWebViewModel>();
+            set.Bind(SupportActionBar).For(v => v.Title).To(vm => vm.Title).Mode(MvxBindingMode.OneWay);
+            set.Apply();
+
+
+            _webView = FindViewById<WebView>(Resource.Id.webView);
+            _webView.Settings.JavaScriptEnabled = true;
+            _webView.Settings.SetSupportZoom(true);
+
+
+            var progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+            var webChromeClient = new ProgressUpdatingWebChromeClient(progressBar);
+            var webViewClient = new ProgressHandlingWebViewClient(progressBar);
+            _webView.SetWebViewClient(webViewClient);
+            _webView.SetWebChromeClient(webChromeClient);
+    
+            _webView.LoadData(((LocalHtmlWebViewModel)ViewModel).Html, "text/html", "UTF-8");
+        }
+    }
+}
