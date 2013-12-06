d008fbd77fe56cc131bc65fd1c418134bae3b05e
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-12-06 12:41:27 +0100

    Adding initial Infoscreen app

---
+++ src/Solvberget.InfoScreen/Solvberget.Infoscreen/app/scenes/Scene1.js
@@ -0,0 +1,70 @@
+alert('SceneScene1.js loaded');
+
+function SceneScene1() {
+
+};
+
+SceneScene1.prototype.initialize = function () {
+	alert("SceneScene1.initialize()");
+	// this function will be called only once when the scene manager show this scene first time
+	// initialize the scene controls and styles, and initialize your variables here
+	// scene HTML and CSS will be loaded before this function is called
+	//window.location='http://80.203.160.221:3535/webapp/#/';
+	$('#svecButton_px3d').sfButton({
+		text:'Start visning'
+	});
+	
+	var tvID = makeid();
+	$('#id-label').html(tvID);
+};
+
+SceneScene1.prototype.handleShow = function (data) {
+	alert("SceneScene1.handleShow()");
+	// this function will be called when the scene manager show this scene
+};
+
+SceneScene1.prototype.handleHide = function () {
+	alert("SceneScene1.handleHide()");
+	// this function will be called when the scene manager hide this scene
+};
+
+SceneScene1.prototype.handleFocus = function () {
+	alert("SceneScene1.handleFocus()");
+	// this function will be called when the scene manager focus this scene
+};
+
+SceneScene1.prototype.handleBlur = function () {
+	alert("SceneScene1.handleBlur()");
+	// this function will be called when the scene manager move focus to another scene from this scene
+};
+
+SceneScene1.prototype.handleKeyDown = function (keyCode) {
+	alert("SceneScene1.handleKeyDown(" + keyCode + ")");
+	// TODO : write an key event handler when this scene get focued
+	switch (keyCode) {
+		case sf.key.LEFT:
+			break;
+		case sf.key.RIGHT:
+			break;
+		case sf.key.UP:
+			break;
+		case sf.key.DOWN:
+			break;
+		case sf.key.ENTER:
+			break;
+		default:
+			alert("handle default key event, key code(" + keyCode + ")");
+			break;
+	}
+};
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
