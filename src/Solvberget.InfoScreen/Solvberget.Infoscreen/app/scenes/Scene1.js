a5d45f694d1db5e3c4a3f7b1d9123f1c71541b99
Dag Olav Prestegarden <dagolav@prestegarden.com>
2013-12-11 15:56:40 +0100

    Cleaning up some of the autogenerated stuff a bit

--- src/Solvberget.InfoScreen/Solvberget.Infoscreen/app/scenes/Scene1.js
+++ src/Solvberget.InfoScreen/Solvberget.Infoscreen/app/scenes/Scene1.js
@@ -1,57 +1,21 @@
-alert('SceneScene1.js loaded');
-
 WEB_APP_URL = "https://www.google.com/search?q=";
 
-function SceneScene1() {
-
-};
+function SceneScene1() { };
 
 SceneScene1.prototype.initialize = function () {
 	alert("SceneScene1.initialize()");
 	// this function will be called only once when the scene manager show this scene first time
 	// initialize the scene controls and styles, and initialize your variables here
 	// scene HTML and CSS will be loaded before this function is called
-	//window.location='http://80.203.160.221:3535/webapp/#/';
 	tvID = this.getid();
 	alert("Current Widget id:" + curWidget.id);
 	$('#id-label').html(tvID);
 };
 
-SceneScene1.prototype.handleShow = function (data) {
-	alert("SceneScene1.handleShow()");
-	// this function will be called when the scene manager show this scene
-};
-
-SceneScene1.prototype.handleHide = function () {
-	alert("SceneScene1.handleHide()");
-	// this function will be called when the scene manager hide this scene
-};
-
-SceneScene1.prototype.handleFocus = function () {
-	alert("SceneScene1.handleFocus()");
-	// this function will be called when the scene manager focus this scene
-};
-
-SceneScene1.prototype.handleBlur = function () {
-	alert("SceneScene1.handleBlur()");
-	// this function will be called when the scene manager move focus to another scene from this scene
-};
-
 SceneScene1.prototype.handleKeyDown = function (keyCode) {
-	alert("SceneScene1.handleKeyDown(" + keyCode + ")");
-	// TODO : write an key event handler when this scene get focused
 	switch (keyCode) {
-		case sf.key.LEFT:
-			break;
-		case sf.key.RIGHT:
-			break;
-		case sf.key.UP:
-			break;
-		case sf.key.DOWN:
-			break;
 		case sf.key.ENTER:
-			document.location = 'http://northrend.bitnexus.net/';
-			//document.location=WEB_APP_URL + tvID;
+			document.location = 'http://solvberget.prestegarden.com/#/'+tvID;
 			break;
 		default:
 			break;
@@ -80,3 +44,8 @@ SceneScene1.prototype.makeid = function()
 
     return text;
 };
+
+SceneScene1.prototype.handleShow = function (data) { };
+SceneScene1.prototype.handleHide = function () { };
+SceneScene1.prototype.handleFocus = function () { };
+SceneScene1.prototype.handleBlur = function () { };
