9c236414bae95438689618ef61a7fdfe2ddd706f
Fredrik Kalseth <fredrik@kalseth.com>
2013-12-12 14:47:28 +0100

    Lagt til FB og Twitter sharing av dokument

--- src/Solvberget/Solvberget.iOS/AppDelegate.cs
+++ src/Solvberget/Solvberget.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using Solvberget.Core.Services;
 using Solvberget.Core.DTOs;
 using Cirrious.MvvmCross.Views;
 using System.Collections.Generic;
+using MonoTouch.FacebookConnect;
 
 namespace Solvberget.iOS
 {
@@ -20,7 +21,10 @@ namespace Solvberget.iOS
     /// </summary>
     [Register("AppDelegate")]
     public class AppDelegate : MvxApplicationDelegate
-    {
+	{
+		const string FacebookAppId = "178416682352295";
+		const string DisplayName = "Sølvberget for iOS";
+
         /// <summary>
         /// The window.
         /// </summary>
@@ -52,8 +56,27 @@ namespace Solvberget.iOS
 
             this.window.MakeKeyAndVisible();
 
+			FBSettings.DefaultAppID = FacebookAppId;
+			FBSettings.DefaultDisplayName = DisplayName;
+
+
             return true;
         }
+
+		public override bool OpenUrl (UIApplication application, NSUrl url, string sourceApplication, NSObject annotation)
+		{
+			// We need to handle URLs by passing them to FBSession in order for SSO authentication
+			// to work.
+			return FBSession.ActiveSession.HandleOpenURL(url);
+		}
+
+		public override void OnActivated (UIApplication application)
+		{
+			// We need to properly handle activation of the application with regards to SSO
+			// (e.g., returning from iOS 6.0 authorization dialog or from fast app switching).
+			FBSession.ActiveSession.HandleDidBecomeActive();
+		}
+
     }
 
 }
--- src/Solvberget/Solvberget.iOS/Views/MediaDetailView.cs
+++ src/Solvberget/Solvberget.iOS/Views/MediaDetailView.cs
@@ -8,6 +8,9 @@ using Solvberget.Core.ViewModels;
 using System.Threading;
 using System.Linq;
 using Solvberget.Core.DTOs;
+using System.Web;
+using MonoTouch.Twitter;
+using MonoTouch.FacebookConnect;
 
 namespace Solvberget.iOS
 {
@@ -81,21 +84,97 @@ namespace Solvberget.iOS
 			});
 		}
 
+		UIBarButtonItem _favButton;
+		UIBarButtonItem _shareButton;
+
 		private void UpdateFavoriteButtonState()
 		{
-			var image = UIImage.FromBundle("/Images/star.on.png").Scale(new SizeF(26, 26));
+			var favStateImage = UIImage.FromBundle("/Images/star.on.png").Scale(new SizeF(26, 26));
 
 			if(!ViewModel.IsFavorite && !UIHelpers.MinVersion7)
 			{
-				image = UIImage.FromBundle("/Images/star.off.png").Scale(new SizeF(26, 26));
+				favStateImage = UIImage.FromBundle("/Images/star.off.png").Scale(new SizeF(26, 26));
 			}
 
-			NavigationItem.SetRightBarButtonItem(new UIBarButtonItem(image, UIBarButtonItemStyle.Plain, OnToggleFavorite), false);
+			if (null == _favButton)
+			{
+				_favButton = new UIBarButtonItem(favStateImage, UIBarButtonItemStyle.Plain, OnToggleFavorite);
+				_shareButton = new UIBarButtonItem(UIBarButtonSystemItem.Action, OnShare);
+
+				NavigationItem.SetRightBarButtonItems(new UIBarButtonItem[]{_shareButton,_favButton}, false);
+			}
 
 			if (UIHelpers.MinVersion7)
 			{
-				NavigationItem.RightBarButtonItem.TintColor = ViewModel.IsFavorite ? Application.ThemeColors.FavoriteColor : Application.ThemeColors.MainInverse;
+				_favButton.TintColor = ViewModel.IsFavorite ? Application.ThemeColors.FavoriteColor : Application.ThemeColors.MainInverse;
 			}
+			else
+			{
+				_favButton.Image = favStateImage;
+			}
+		}
+
+		private void OnShare(object sender, EventArgs e)
+		{
+			var shareView = new UIAlertView(View.Frame);
+
+			shareView.Title = "Del " + ViewModel.Title;
+
+			shareView.AddButton("Del på Facebook");
+			shareView.AddButton("Del på Twitter");
+			shareView.AddButton("Send på E-post");
+			shareView.AddButton("Avbryt");
+
+			shareView.Clicked += (ss, se) =>
+			{
+				string launchUri = null;
+
+				var shareMessage = "Se hva jeg fant på Sølvberget: " + ViewModel.Title;
+
+				if(null == ViewModel.RawDto.WebUrl) return;
+
+				switch(se.ButtonIndex)
+				{
+					case 0:
+
+
+						if(!FBDialogs.CanPresentOSIntegratedShareDialog(FBSession.ActiveSession))
+						{
+							UIAlertView alert = new UIAlertView(View.Frame);
+							alert.Title = "Facebook oppsett mangler";
+							alert.Message = "Du må koble din iPhone/iPad til Facebook før du kan dele (selv om du kanskje har installet Facebook appen). Gå til Instillinger - Facebook.";
+							alert.AddButton("Ok");
+							alert.Show();
+							return;
+						}
+
+						FBDialogs.PresentOSIntegratedShareDialogModally(this,
+							shareMessage, null, new NSUrl(ViewModel.RawDto.WebUrl),new FBOSIntegratedShareDialogHandler((res,err) => {
+
+								var ex = err;
+
+							}));
+
+
+						break;
+					case 1:
+
+						var tvc = new TWTweetComposeViewController();
+						tvc.SetInitialText(shareMessage);
+						tvc.AddUrl(new NSUrl(ViewModel.RawDto.WebUrl));
+						PresentModalViewController(tvc, true);
+
+						break;
+				}
+
+				if(null == launchUri) return;
+
+				bool success = UIApplication.SharedApplication.OpenUrl(new NSUrl(launchUri));
+			};
+
+			shareView.CancelButtonIndex = shareView.ButtonCount - 1;
+
+			shareView.Show();
 		}
 
 		private void Update()
